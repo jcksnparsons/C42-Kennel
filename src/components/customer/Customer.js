586fa9ea009c6e152d14db832996d05a028db1d9
Jack Parsons <jcksnparsons@yahoo.com>
2020-09-08 10:13:18 -0500

    made cards for locations, employees, and customers. about to refactor css

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css"
+
+export const Customer = () => (
+  <section className="customer">
+    <h3 className="customer__name">Nandor The Relentless</h3>
+    <div className="customer__address">Address: Ashley St., Staten Island</div>
+  </section>
+);
---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Employee.css"
+
+export const Employee = () => (
+  <section className="employee">
+    <h3 className="employee__name">Lazlo Cravensworth</h3>
+    <div className="employee__location">East Nashville</div>
+  </section>
+);
