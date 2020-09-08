d9be6f779e5977d31f4d3ec3a79f5a3eabe38479
Jack Parsons <jcksnparsons@yahoo.com>
2020-09-08 14:09:58 -0500

    added navbar, refactored

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,43 +1,55 @@
-import React from "react";
-import { LocationList } from "./location/LocationList";
-import { LocationProvider } from "./location/LocationProvider";
-import { Employee } from "./employee/Employee";
-import { CustomerProvider } from "./customer/CustomerProvider";
-import { CustomerList } from "./customer/CustomerList";
-import "./Kennel.css";
-import { AnimalProvider } from "./animal/AnimalProvider";
-import { AnimalList } from "./animal/AnimalList";
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-import { EmployeeList} from "./employee/EmployeeList"
+import React from "react"
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css"
 
 export const Kennel = () => (
-  <>
-    <h2>Nashville Kennels</h2>
-    <small>Loving care when you're not there.</small>
+    <>
+        <NavBar />
+        <ApplicationViews />
+    </>
+)
 
-    <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
-    </address>
+// import React from "react";
+// import { LocationList } from "./location/LocationList";
+// import { LocationProvider } from "./location/LocationProvider";
+// import { CustomerProvider } from "./customer/CustomerProvider";
+// import { CustomerList } from "./customer/CustomerList";
+// import "./Kennel.css";
+// import { AnimalProvider } from "./animal/AnimalProvider";
+// import { AnimalList } from "./animal/AnimalList";
+// import { EmployeeProvider } from "./employee/EmployeeProvider"
+// import { EmployeeList} from "./employee/EmployeeList"
 
-    <h2>Animals</h2>
-    <AnimalProvider>
-      <AnimalList />
-    </AnimalProvider>
+// export const Kennel = () => (
+//   <>
+//     <h2>Nashville Kennels</h2>
+//     <small>Loving care when you're not there.</small>
 
-    <h2>Locations</h2>
-    <LocationProvider>
-      <LocationList />
-    </LocationProvider>
+//     <address>
+//       <div>Visit Us at the Nashville North Location</div>
+//       <div>500 Puppy Way</div>
+//     </address>
 
-    <h2>Employees</h2>
-    <EmployeeProvider>
-      <EmployeeList />
-    </EmployeeProvider>
+//     <h2>Animals</h2>
+//     <AnimalProvider>
+//       <AnimalList />
+//     </AnimalProvider>
 
-    <h2>Customers</h2>
-    <CustomerProvider>
-      <CustomerList />
-    </CustomerProvider>
-  </>
-);
+//     <h2>Locations</h2>
+//     <LocationProvider>
+//       <LocationList />
+//     </LocationProvider>
+
+//     <h2>Employees</h2>
+//     <EmployeeProvider>
+//       <EmployeeList />
+//     </EmployeeProvider>
+
+//     <h2>Customers</h2>
+//     <CustomerProvider>
+//       <CustomerList />
+//     </CustomerProvider>
+//   </>
+// );
